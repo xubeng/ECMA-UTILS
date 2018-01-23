6ccb4df1818db6af64d8185f3641fb3f821109ce
xubeng <1138993977@qq.com>
2018-01-23 14:11:12 +0800

    commit by command

---
+++ String/findLongestWord.js
@@ -0,0 +1,12 @@
+
+<!--  查找语句中最长的单词 -->
+function findLongestWord(str) {
+  var words = str.split(' ');
+  var maxLength = 0;
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+  return maxLength;
+}
