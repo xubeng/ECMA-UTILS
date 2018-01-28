7824a667f0722fa3c96575f27bfb1fa9eed35a56
xubeng <1138993977@qq.com>
2018-01-28 23:49:40 +0800

    delete not perfect

--- String/titleCase.js
+++
@@ -1,7 +0,0 @@
-function titleCase(str) {
-  var convertToArray = str.toLowerCase().split(" ");
-  var result = convertToArray.map(function(val){
-      return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
-  });
-  return result.join(" ");
-}
